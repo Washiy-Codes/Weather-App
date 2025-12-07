cb47815b5fd8f47e0107223109c02943afd3ef1c
Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2025-12-07 16:09:23 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Weather from "./Weather"
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Weather Search App</h1>
+      
+      <Weather />
+    </>
+  )
+}
+
+export default App
